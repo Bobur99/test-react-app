869bfdb0b73d7585869c3a0f060f588a1d21b420
coder <coder>
2022-01-16 03:10:12 +0500

    test

---
+++ src/pages/Profile.js
@@ -0,0 +1,94 @@
+import React, { useContext, useEffect, Fragment } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Repos } from "../components/Repos";
+import { GithubContext } from "../context/github/githubContext";
+
+export const Profile = () => {
+  const { getUser, getRepos, loading, user, repos } = useContext(GithubContext);
+  let match = useParams();
+  const urlName = match.name;
+  // const {name:urlName} = useParams()
+  // console.log(urlName);
+  useEffect(() => {
+    getUser(urlName);
+    getRepos(urlName);
+    // eslint-disable-next-line
+  }, []);
+
+  if (loading) {
+    return <p className="text-center">Загрузка...</p>;
+  }
+
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gist,
+  } = user;
+
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-link">
+        На главную
+      </Link>
+      <div className="card mb-4">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-md-3 col-md-2 text-center">
+              <img src={avatar_url} alt={name} style={{ width: "150px" }} />
+              <h1>{name}</h1>
+              {location && <p>Местоположение: {location}</p>}
+            </div>
+            <div className="col">
+              {bio && (
+                <Fragment>
+                  <h3>BIO</h3>
+                  <p>{bio}</p>
+                </Fragment>
+              )}
+              <a
+                href={html_url}
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-dark"
+              >
+                Открыть профиль
+              </a>
+              <ul>
+                {login && (
+                  <li>
+                    <strong>Username: </strong> {login}
+                  </li>
+                )}
+                {company && (
+                  <li>
+                    <strong>Компания: </strong> {company}
+                  </li>
+                )}
+                {blog && (
+                  <li>
+                    <strong>Website: </strong> {blog}
+                  </li>
+                )}
+              </ul>
+
+              <div className="badge bg-primary">Подписчики: {followers}</div>
+              <div className="badge bg-success">Подписан: {following}</div>
+              <div className="badge bg-info">Репозитории: {public_repos}</div>
+              <div className="badge bg-dark">Gists: {public_gist}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
